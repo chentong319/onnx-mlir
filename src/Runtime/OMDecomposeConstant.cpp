9eb4d94aa80c37cd320d0d20d89f44d05ebfb171
chentong319 <chentong@us.ibm.com>
2023-07-14 18:10:17 -0400

    run a particular constant
    Signed-off-by: chentong319 <chentong@us.ibm.com>

---
+++ src/Runtime/OMDecomposeConstant.cpp
@@ -0,0 +1,15 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+//===----- OMDecomposeConstant.c - OMDecomposeConstant C Implementation ---===//
+//
+// Copyright 2022 The IBM Research Authors.
+//
+// =============================================================================
+//
+// This file contains implementation of the OMDecomposeConstant functions.
+//
+//===----------------------------------------------------------------------===//
+
+#include "OMDecomposeConstant.inc"
