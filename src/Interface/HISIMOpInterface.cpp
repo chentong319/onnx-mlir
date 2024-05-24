56eda20b23bd8013fee12efd2829afb5ce0b7f13
chentong319 <chentong@us.ibm.com>
2024-05-24 16:15:03 -0400

    add interface
    Signed-off-by: chentong319 <chentong@us.ibm.com>

---
+++ src/Interface/HISIMOpInterface.cpp
@@ -0,0 +1,23 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+//===---- HISIMOpInterface.cpp - Definition for HISIM ---===//
+//
+// Copyright 2024 The IBM Research Authors.
+//
+// =============================================================================
+//
+// This file contains the implementations of the HISIM Op interfaces
+// defined in HISIMInterface.td.
+//
+//===----------------------------------------------------------------------===//
+
+#include "src/Interface/HISIMOpInterface.hpp"
+
+namespace mlir {
+
+/// Include the auto-generated declarations.
+#include "src/Interface/HISIMOpInterface.cpp.inc"
+
+} // end namespace mlir
---
+++ src/Interface/HISIMOpInterface.hpp
@@ -0,0 +1,22 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+//===---- HISIMOpInterface.hpp - Definition for HISIM ---===//
+//
+// Copyright 2024 The IBM Research Authors.
+//
+// =============================================================================
+//
+// This file contains the declarations of the HISIM op interfaces defined
+// in HISIMInterface.td.
+//
+//===----------------------------------------------------------------------===//
+
+#pragma once
+
+#include "mlir/IR/BuiltinOps.h"
+#include "mlir/IR/OpDefinition.h"
+
+/// Include the auto-generated declarations.
+#include "src/Interface/HISIMOpInterface.hpp.inc"
